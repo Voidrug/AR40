8a69bc7988329f8c00bd924236875c827e98f946
시문 김 <rlatlans13@naver.com>
2022-01-17 13:51:03 +0900

    김시문 :  Homework

--- Homework/Homework20220114_TextChange/Homework20220114_TextChange.cpp
+++ Homework/Homework20220114_TextChange/Homework20220114_TextChange.cpp
@@ -3,16 +3,57 @@
 
 void TextChange(char* _Text, const char* _Prev, const char* _Next)
 {
+	int TextCount = 0;
+	int PrevCount = 0;
+	int NextCount = 0;
+	int TrueCount = 0;
 
+	while (0 != _Prev[PrevCount])
+	{
+		PrevCount += 1;
+	}
+	while (0 != _Next[NextCount])
+	{
+		NextCount += 1;
+	}
+
+	while (0 != _Text[TextCount])
+	{
+		TrueCount = 0;
+
+		for (int i = 0; i < PrevCount; i++)
+		{
+			if (_Text[TextCount + i] == _Prev[i])
+			{
+				TrueCount += 1;
+			}
+
+			if (TrueCount == PrevCount)
+			{
+				for (int j = 0; j < NextCount; j++)
+				{
+					_Text[TextCount+j] = _Next[j];
+				}
+
+				TextCount += PrevCount;
+			}
+			
+		}
+
+		TextCount += 1;
+
+	}
+	
+	return;
 }
 
 int main()
 {
 	{
-		char Text[100] = "aa, bb, aa, bb aa aa aa aaaaaaa ccdffeds";
+		char Text[100] = "aa, bb, aa, bb aa aa aa aaaaaaa ccaffeds";
 
 		TextChange(Text, "aa", "fff");
-		// "fff, bb, fff, bb fff fff fff fffffffffa ccdffeds";
+		// "fff, bb, fff, bb fff fff fff fffffffffa ccaffeds";
 		printf_s(Text);
 	}
 
