3cccf8d61dee6e2f74400d642d384369600cf7d4
시문 김 <rlatlans13@naver.com>
2022-01-17 11:45:57 +0900

    김시문 : Homework

--- Homework/Homework20220114_TextChange/Homework20220114_TextChange.cpp
+++ Homework/Homework20220114_TextChange/Homework20220114_TextChange.cpp
@@ -3,24 +3,7 @@
 
 void TextChange(char* _Text, const char* _Prev, const char* _Next)
 {
-	int Count = 0;
 
-	while (0 != _Text[Count])
-	{
-		if (_Prev[0] == _Text[Count] && _Prev[1] == _Text[Count + 1])
-		{
-			_Text[Count] = _Next[0];
-			_Text[Count + 1] = _Next[1];
-			_Text[Count + 2] = _Next[2];
-
-			Count += 1;
-		}
-
-		Count += 1;
-
-	}
-	
-	return;
 }
 
 int main()
