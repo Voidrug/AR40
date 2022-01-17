a91d5f2bfe930ad8f7925b9859cc5d7d3c407e4c
시문 김 <rlatlans13@naver.com>
2022-01-17 14:59:00 +0900

    김시문 : Homework

--- Homework/Homework20220114_TextChange/Homework20220114_TextChange.cpp
+++ Homework/Homework20220114_TextChange/Homework20220114_TextChange.cpp
@@ -26,25 +26,25 @@ void TextChange(char* _Text, const char* _Prev, const char* _Next)
 
 		// 선택된 한 글자 기준으로 변경될 텍스트 길이만큼 확인하기 
 		for (int i = 0; i < PrevCount; i++)
-		{	
-			// 한글자가 일치할때마다 TrueCount를 1씩 올림
+		{
+			// 한 글자가 일치할때마다 TrueCount를 1씩 올림
 			if (_Text[TextCount + i] == _Prev[i])
 			{
 				TrueCount += 1;
 			}
 
-			// 변경될 글자수 = 일치하는 글자수(TrueCount)가 같으면 변경을 실행함
-			if (TrueCount == PrevCount)
-			{
-				for (int j = 0; j < NextCount; j++)
-				{
-					_Text[TextCount+j] = _Next[j];
-				}
+		}
 
-				// 변경된 글자수만큼 칸수를 옮겨서 시작하기 위해 값을 더해줌
-				TextCount += PrevCount;
+		// 변경될 글자수 = 일치하는 글자수(TrueCount)가 같으면 변경을 실행함
+		if (TrueCount == PrevCount)
+		{
+			for (int j = 0; j < NextCount; j++)
+			{
+				_Text[TextCount + j] = _Next[j];
 			}
-			
+
+			// 변경된 글자수만큼 칸수를 옮겨서 시작하기 위해 값을 더해줌
+			TextCount += PrevCount;
 		}
 
 		//안쪽이 실행하든 안하든 칸수를 옮겨 다음 글자를 확인하기 위해 값을 더해줌
