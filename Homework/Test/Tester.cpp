6e67ebd68af3c96557540e6f76f7e64cec7f6b41
시문 김 <rlatlans13@naver.com>
2022-02-22 10:06:21 +0900

    김시문 : Homework 20220222

---
+++ Homework/Test/Tester.cpp
@@ -0,0 +1,42 @@
+#include "Tester.h"
+#include <iostream>
+
+class Unit
+{
+public:
+
+	int Att;
+	int Def;
+	int HP;
+
+	Unit()
+		:Att(0)
+		,Def(0)
+		,HP(0)
+	{
+	}
+
+	~Unit()
+	{
+	}
+};
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
